10d15efe5df00303b72298ecad1d133edcecc60f
Francis Wilkinson <2400francis@gmail.com>
2024-03-13 01:37:22 +0000

    edited contact information and Navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="FrancisWilkinson.pdf">
                 Resume 
               </a>
             </li>
