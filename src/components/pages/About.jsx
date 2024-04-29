35e10dfa4cd692e41293b02afeb991930e6e5676
Francis Wilkinson <2400francis@gmail.com>
2024-04-29 23:23:10 +0100

    Edited the About Page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -9,7 +9,7 @@ function About() {
     <div className='container'>
       <Wrapper>
       <Title>About</Title>
-      <p>Web Developer with a Bachelor of Science in Mathematics, a double minor in Computer Science and Biological Sciences, and a background in Web Development. I have recently been doing a 16 week bootcamp in Frontend Wed Development to learn more Javascript and modern forms of Web Development.</p>
+      <p>Trainee Full Stack Web Developer with a Bachelor of Science in Mathematics, a double minor in Computer Science and Biological Sciences, and a work background in Web Design/Development. I have recently been doing a 16 week bootcamp in Frontend Web Development to learn more Javascript and modern forms of Web Development.</p>
       <JSXVariables />
       <p>
         HTML5, CSS, Javascript, Git, Bootstrap, jQuery, NodeJS, React
